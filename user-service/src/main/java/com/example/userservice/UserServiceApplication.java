5bd43da9562120a2f143a04df00f6f8c127f6d79
LEEJAECHEOL <jae_choel@naver.com>
2021-07-21 20:35:20 +0900

    2. User-Service 등록-2

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -13,3 +13,14 @@ public class UserServiceApplication {
   }
 
 }
+
+/**
+ * 명령어로 서버 추가 실행
+ *  - mvn spring-boot:run -Dspring-boot.run.jvmArguments='-Dserver.port=9003'
+ *
+ * cmd
+ *  - mvn clean (target 삭제)
+ *  - mvn compile package (컴파일 후 패키지 생성)
+ *  - 빌드 성공 후 jar 파일이 만들어짐
+ *  - jar 실행 : java -jar -Dserver.port=9004 ./target/user-service-0.0.1-SNAPSHOT.jar
+ */

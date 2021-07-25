57154f076b12e79eb18055340745fd5bb224ec7a
LEEJAECHEOL <jae_choel@naver.com>
2021-07-25 17:47:34 +0900

    gateway loadbalancer

--- first-service/src/main/java/com/example/firstservice/FirstController.java
+++ first-service/src/main/java/com/example/firstservice/FirstController.java
@@ -1,15 +1,25 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
 @RequestMapping("/first-service")
 public class FirstController {
+  Environment environment;
+
+  @Autowired
+  public FirstController(Environment environment) {
+    this.environment = environment;
+  }
 
   @GetMapping("/welcome")
   public String welcom(){
@@ -23,7 +33,10 @@ public class FirstController {
   }
 
   @GetMapping("/check")
-  public String check(){
-    return "Hi, there. this is a message from Fist Service";
+  public String check(HttpServletRequest request){
+    log.info("Server port = {}", request.getServerPort());
+
+    return String.format("Hi, there. there is a message from Fist Service on Port %s",
+                            environment.getProperty("local.server.port"));
   }
 }

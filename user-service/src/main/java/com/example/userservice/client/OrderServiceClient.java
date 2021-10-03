109e70bc86463c6e6da2866b4f6e8be49b8b1e7f
LEEJAECHEOL <jae_choel@naver.com>
2021-10-03 18:04:24 +0900

    feign url 수정

--- user-service/src/main/java/com/example/userservice/client/OrderServiceClient.java
+++ user-service/src/main/java/com/example/userservice/client/OrderServiceClient.java
@@ -10,7 +10,7 @@ import java.util.List;
 @FeignClient(name = "order-service") // 호출할 마이크로 서비스 이름
 public interface OrderServiceClient {
 
-  @GetMapping("/order-service/{userId}/orders_ng")
-//  @GetMapping("/order-service/{userId}/orders")
+//  @GetMapping("/order-service/{userId}/orders_ng")
+  @GetMapping("/order-service/{userId}/orders")
   List<ResponseOrder> getOrders(@PathVariable String userId);
 }

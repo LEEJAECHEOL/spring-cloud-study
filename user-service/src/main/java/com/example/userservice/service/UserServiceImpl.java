ee4f4b55cd257c69df0642efc71e0daa03443640
LEEJAECHEOL <jae_choel@naver.com>
2021-10-03 17:52:48 +0900

    마이크로 서비스간 통신-Feign

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.example.userservice.service;
 
+import com.example.userservice.client.OrderServiceClient;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -27,8 +29,9 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService{
   private final UserRepository userRepository;
   private final BCryptPasswordEncoder bCryptPasswordEncoder;
-  private final RestTemplate restTemplate;
-  private final Environment env;
+//  private final RestTemplate restTemplate;
+//  private final Environment env;
+  private final OrderServiceClient orderServiceClient;
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -79,13 +82,26 @@ public class UserServiceImpl implements UserService{
 //    List<ResponseOrder> orders = new ArrayList<>();
 
     /* Using Rest Template */
-    String orderUrl = String.format(env.getProperty("order_service.url"), userId);
+//    String orderUrl = String.format(env.getProperty("order_service.url"), userId);
+//
+//    ResponseEntity<List<ResponseOrder>> orderListResponse =
+//      restTemplate.exchange(orderUrl, HttpMethod.GET, null,
+//        new ParameterizedTypeReference<List<ResponseOrder>>() {
+//    });
+//    List<ResponseOrder> orderList = orderListResponse.getBody();
 
-    ResponseEntity<List<ResponseOrder>> orderListResponse =
-      restTemplate.exchange(orderUrl, HttpMethod.GET, null,
-        new ParameterizedTypeReference<List<ResponseOrder>>() {
-    });
-    List<ResponseOrder> orderList = orderListResponse.getBody();
+    /* Using Feign Client */
+    /* Feign Exception handling */
+//    List<ResponseOrder> orderList = null;
+//    try {
+//      orderList = orderServiceClient.getOrders(userId);
+//    }catch (FeignException e){
+//      e.printStackTrace();
+//    }
+
+    /* Error Decoder */
+    List<ResponseOrder> orderList = orderServiceClient.getOrders(userId);
+
     userDto.setOrders(orderList);
 
     return userDto;
